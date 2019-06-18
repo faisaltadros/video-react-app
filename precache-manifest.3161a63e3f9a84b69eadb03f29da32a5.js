6841203f9a277469f41ac4c04841a1ef6da874bd
Faisal <faisal@rotatestudios.com>
2019-06-17 21:37:46 -0500

    Updates

---
+++ precache-manifest.3161a63e3f9a84b69eadb03f29da32a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2543671fa58ee0592fa9aabf7ffda64f",
+    "url": "/video-react-app/index.html"
+  },
+  {
+    "revision": "dd4ff9c35ba33f229fb4",
+    "url": "/video-react-app/static/css/main.5d06aabd.chunk.css"
+  },
+  {
+    "revision": "6237651f7e9f2c0973b1",
+    "url": "/video-react-app/static/js/2.e202049d.chunk.js"
+  },
+  {
+    "revision": "dd4ff9c35ba33f229fb4",
+    "url": "/video-react-app/static/js/main.21175fa7.chunk.js"
+  },
+  {
+    "revision": "9b57fee3e549235541a6",
+    "url": "/video-react-app/static/js/runtime~main.40c16af7.js"
+  }
+]);
